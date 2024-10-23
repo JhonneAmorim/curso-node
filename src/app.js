9cc763cbbb85bd1a63d8dba41f9406e5ea5ba7dc
JhonneAmorim <jhonneamorimdev@gmail.com>
2024-10-23 14:27:33 -0300

    feat: Curso node#06 - Rotas: Middleware

--- src/app.js
+++ src/app.js
@@ -6,7 +6,18 @@ const express = require("express");
 
 const app = express();
 
+// app.use('/signup', function (req, res, next) {
+//     // res.json("middleware");
+//     // req.name = 'John Doe';
+//     // req.age = 25;
+//     return res.json("signup");
+//     // next();
+// });
+
 app.get("/", Home.index);
-app.post("/login", Login.store);
+app.post("/login", function (req, res, next) {
+    return res.json("middleware login");
+    // next();
+}, Login.store);
 
 app.listen(process.env.PORT || 3000);
--- src/controllers/Home.js
+++ src/controllers/Home.js
@@ -1,5 +1,5 @@
 const index = function (req, res) {
-    return res.json("home");
+    return res.json({ name: req.name, age: req.age });
 }
 
 module.exports = { index };
